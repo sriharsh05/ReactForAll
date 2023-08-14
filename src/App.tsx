c7e9e725b0e3fcf5db5059373884c92ce686dbdc
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-08-14 21:16:33 +0530

    branched milestone-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import AppContainer from "./components/AppContainer";
-import { Home } from "./components/Home";
-import Form from "./components/Form";
+import React from "react";
+import Header from "./Header";
+import AppContainer from "./AppContainer";
 
-
+const formFildes = [
+  { id: 1, label: "First Name", type: "text" },
+  { id: 2, label: "Last Name", type: "text" },
+  { id: 3, label: "Email", type: "text" },
+  { id: 3, label: "Date of Birth", type: "date" },
+  { id: 3, label: "Phone Number", type: "tel" },
+];
 
 function App() {
-  const [state, setState] = useState("HOME");
-
-  const openForm = () =>{
-    setState("FORM");
-  }
-  
-  const closeForm = () =>{
-    setState("HOME");
-  } 
-
   return (
     <AppContainer>
-      <div className="p-4 mx-auto bg-white shadow-lg rounded-xl">
+      <div className="p-4 mx-auto bg-stone-200 shadow-lg rounded-xl">
         <Header
           title={"Welcome to Lesson 5 of $react-typescript with #tailwindcss"}
         />
-        {state === "HOME" ? (
-            <Home openFormCB={openForm} />
-        ) : (
-            <Form closeFormCB={closeForm} />
-        )}
+        {formFildes.map((field) => (
+          <React.Fragment key={field.id}>
+            <label>{field.label}</label>
+            <input
+              className="block text-gray-500 border-2 border-gray-300 rounded-lg p-2 m-2 w-full"
+              type={field.type}
+            />
+          </React.Fragment>
+        ))}
+        <button className="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full">
+          Submit
+        </button>
       </div>
     </AppContainer>
   );
 }
 
 export default App;
- 
