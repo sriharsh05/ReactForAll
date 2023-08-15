3839606ae958677c8482f5e8f1dd51a9f60b1137
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-08-15 12:50:08 +0530

    add clear form feature

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import LabeledInput from "./LabeledInput";
 
 const formFields = [
-  { id: 1, label: "First Name", type: "text" },
-  { id: 2, label: "Last Name", type: "text" },
-  { id: 3, label: "Email", type: "text" },
-  { id: 4, label: "Date of birth", type: "date" },
+  { id: 1, label: "First Name", type: "text", value: "" },
+  { id: 2, label: "Last Name", type: "text", value: "" },
+  { id: 3, label: "Email", type: "text", value: "" },
+  { id: 4, label: "Date of birth", type: "date", value: "" },
 ];
 
 export default function Form(props: { closeFormCB: () => void }) {
@@ -18,12 +18,38 @@ export default function Form(props: { closeFormCB: () => void }) {
         id: Number(new Date()),
         label: newField,
         type: "text",
+        value: "",
       },
     ]);
     setNewField("New Value");
   };
+
   const removeField = (id: number) => {
-    setState(state.filter((field) => field.id !== id));
+    setState(
+      state.filter(
+        (field) => field.id !== id
+        )
+      );
+  };
+  const setValue = (id: number, value: string) => {
+    setState(
+      state.map((field) => {
+        if (field.id === id) {
+          return {
+            ...field,
+            value,
+          };
+        }
+        return field;
+      })
+    );
+  };
+  const clearForm = () => {
+    setState(
+      state.map(
+        (field) => ({ ...field, value: "" })
+        )
+      );
   };
   return (
     <div className="flex flex-col gap-2 p-4 divide-y divide-dotted">
@@ -33,7 +59,9 @@ export default function Form(props: { closeFormCB: () => void }) {
             id={field.id}
             label={field.label}
             type={field.type}
+            value={field.value}
             removeFieldCB={removeField}
+            setValueCB={setValue}
           />
         ))}
       </div>
@@ -41,8 +69,8 @@ export default function Form(props: { closeFormCB: () => void }) {
         <input
           type="text"
           className="border-2 border-gray-300 rounded-lg p-2 my-2 flex-1"
-          value = {newField}
-          onChange={e=> {
+          value={newField}
+          onChange={(e) => {
             setNewField(e.target.value);
           }}
         />
@@ -62,6 +90,12 @@ export default function Form(props: { closeFormCB: () => void }) {
           onClick={props.closeFormCB}
         >
           Close Form
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-4 rounded-lg"
+          onClick={clearForm}
+        >
+          Clear Form
         </button>
       </div>
     </div>
