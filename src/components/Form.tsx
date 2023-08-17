73dff7fbb5b81d5426a736f156031b6fbfedf8ac
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-08-17 11:33:34 +0530

    add formData interface and made changes accordingly, used useref hook for focus on title

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import LabeledInput from "./LabeledInput";
+
+interface formData {
+  title: string;
+  formFields : formField[];
+}
 
 interface formField {
   id: number;
@@ -15,25 +20,29 @@ const initialFormFields: formField[] = [
   { id: 4, label: "Date of birth", type: "date", value: "" },
 ];
 
-const initialState: () => formField[] = () => {
-  const formFieldsJSON = localStorage.getItem("formFields");
+const initialState: () => formData = () => {
+  const formFieldsJSON = localStorage.getItem("formData");
   const persistantFormFields = formFieldsJSON
     ? JSON.parse(formFieldsJSON)
-    : initialFormFields;
+    :{
+        title : "untitled form",
+        formFields :initialFormFields
+      };
   return persistantFormFields;
 };
 
-const saveFormData = (currentState: formField[]) => {
-  localStorage.setItem("formFields", JSON.stringify(currentState));
+const saveFormData = (currentState: formData) => {
+  localStorage.setItem("formData", JSON.stringify(currentState));
 };
 
 export default function Form(props: { closeFormCB: () => void }) {
   const [state, setState] = useState(initialState());
   const [newField, setNewField] = useState("");
+  const titleRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     console.log("Component Mounted");
     document.title = "Form Editor";
-
+    titleRef.current?.focus();
     return () => {
       document.title = "React App";
     };
@@ -49,24 +58,31 @@ export default function Form(props: { closeFormCB: () => void }) {
   }, [state]);
 
   const addField = () => {
-    setState([
+    setState({
       ...state,
-      {
-        id: Number(new Date()),
-        label: newField,
-        type: "text",
-        value: "",
-      },
-    ]);
-    setNewField("New Value");
+      formFields: [
+        ...state.formFields,
+        {
+          id: Number(new Date()),
+          label: newField,
+          type: "text",
+          value: "",
+        },
+      ]
+    });
+    setNewField("");
   };
 
   const removeField = (id: number) => {
-    setState(state.filter((field) => field.id !== id));
+    setState({
+      ...state,
+      formFields : state.formFields.filter((field) => field.id !== id)
+    });
   };
   const setValue = (id: number, value: string) => {
-    setState(
-      state.map((field) => {
+    setState({
+      ...state,
+      formFields: state.formFields.map((field) => {
         if (field.id === id) {
           return {
             ...field,
@@ -75,15 +91,31 @@ export default function Form(props: { closeFormCB: () => void }) {
         }
         return field;
       })
+    }   
     );
   };
   const clearForm = () => {
-    setState(state.map((field) => ({ ...field, value: "" })));
+    setState({
+      ...state,
+      formFields: state.formFields.map((field) => ({
+        ...field,
+        value: "",
+      })),
+    });
   };
   return (
     <div className="flex flex-col gap-2 p-4 divide-y divide-dotted">
+      <input
+          type="text"
+          className="border-2 border-gray-300 rounded-lg p-2 my-2 flex-1"
+          value={state.title}
+          onChange={(e) => {
+            setState({...state, title: e.target.value });
+          }}
+          ref = {titleRef}
+        />
       <div>
-        {state.map((field) => (
+        {state.formFields.map((field) => (
           <LabeledInput
             id={field.id}
             label={field.label}
