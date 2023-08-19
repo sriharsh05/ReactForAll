4c3527c4926a26b7725f4874cf50bd6f31067a35
sriharsh05 <sriharsh.harsha5@gmail.com>
2023-08-19 15:57:39 +0530

    removed colsole errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,21 @@ import Header from "./Header";
 import AppContainer from "./AppContainer";
 import LabeledInput from "./LabeledInput";
 
-const formFildes = [
+const formFields = [
   { id: 1, label: "First Name", type: "text" },
   { id: 2, label: "Last Name", type: "text" },
-  { id: 3, label: "Email", type: "text" },
-  { id: 3, label: "Date of Birth", type: "date" },
-  { id: 3, label: "Phone Number", type: "tel" },
+  { id: 3, label: "Email", type: "email" },
+  { id: 4, label: "Date of Birth", type: "date" },
+  { id: 5, label: "Phone Number", type: "tel" },
 ];
 
 function App() {
   return (
     <AppContainer>
       <div className="p-4 mx-auto bg-stone-200 shadow-lg rounded-xl">
-        <Header
-          title={"Welcome to Lesson 5 of $react-typescript with #tailwindcss"}
-        />
-        {formFildes.map((field) => (
-          <>
-            <LabeledInput id={field.id} label={field.label} type={field.type} />
-          </>
+        <Header title={"Welcome to Lesson 5 of $react-typescript with #tailwindcss"} />
+        {formFields.map((field) => (
+          <LabeledInput key={field.id} id={field.id} label={field.label} type={field.type} />
         ))}
         <button className="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full">
           Submit
